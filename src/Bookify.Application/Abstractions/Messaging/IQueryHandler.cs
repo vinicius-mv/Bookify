4093c6efc95bc72aa0251c3cae91b16470d9a8a3
VinciusVieira <viniciusmv91@gmail.com>
2024-04-29 23:14:58 -0300

    Add GetBooking use case, also add Dappe to the project

--- src/Bookify.Application/Abstractions/Messaging/IQueryHandler.cs
+++ src/Bookify.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -3,11 +3,8 @@ using MediatR;
 
 namespace Bookify.Application.Abstractions.Messaging;
 
-public class IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
     where TQuery : IQuery<TResponse>
 {
-    public Task<Result<TResponse>> Handle(TQuery request, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<Result<TResponse>> Handle(TQuery request, CancellationToken cancellationToken);
 }
---
+++ src/Bookify.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,8 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+public sealed record class GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>
+{
+
+}
