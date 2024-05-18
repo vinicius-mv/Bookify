6fe255e4786b4c023356b04ddf2ece5a7a566299
“Vinicius <viniciusmv91@gmail.com>
2024-05-18 19:04:23 -0300

    Configuring services with dependency injection

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,3 +1,6 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
