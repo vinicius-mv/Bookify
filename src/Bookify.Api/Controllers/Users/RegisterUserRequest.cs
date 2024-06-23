48cfd1c0bca6a0e0a5af2fc8b341d739ad78c440
Vinicius Vieira <viniciusmv91@gmail.com>
2024-06-22 23:21:30 -0300

    Add API endpoint for registering a user

---
+++ src/Bookify.Api/Controllers/Users/RegisterUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Api.Controllers.Users
+{
+    public sealed record RegisterUserRequest(
+        string Email,
+        string FirstName,
+        string LastName,
+        string Password);
+}
