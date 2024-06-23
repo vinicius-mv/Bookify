d123ad59a6a96554439b6fa13a7fa242ccfa6830
Vinicius Vieira <viniciusmv91@gmail.com>
2024-06-23 00:18:41 -0300

    Add authentication service

---
+++ src/Bookify.Infrastructure/Authentication/AdminAuthorizationDelegatingHandler.cs
@@ -0,0 +1,61 @@
+﻿using Bookify.Infrastructure.Authentication.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Bookify.Infrastructure.Authentication;
+
+public sealed class AdminAuthorizationDelegatingHandler : DelegatingHandler
+{
+    private readonly KeycloakOptions _keyclokOptions;
+
+    public AdminAuthorizationDelegatingHandler(IOptions<KeycloakOptions> keycloakOptions)
+    {
+        
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken = default)
+    {
+        var authrorizationToken = await GetAuthorizationToken(cancellationToken);
+
+        request.Headers.Authorization = new AuthenticationHeaderValue(
+            JwtBearerDefaults.AuthenticationScheme,
+            authrorizationToken.AccessToken);
+
+        var httpResponseMessage = await base.SendAsync(request, cancellationToken);
+
+        httpResponseMessage.EnsureSuccessStatusCode();
+
+        return httpResponseMessage;
+    }
+
+    private async Task<AuthorizationToken> GetAuthorizationToken(CancellationToken cancellationToken)
+    {
+        var authorizationRequestParameters = new KeyValuePair<string, string>[]
+        {
+            new("client_id", _keyclokOptions.AdminClientId),
+            new("client_secret", _keyclokOptions.AdminClientSecret),
+            new("scope", "openid email"),
+            new("grant_type", "client_credentials")
+        };
+
+        var authorizationRequestContent = new FormUrlEncodedContent(authorizationRequestParameters);
+
+        var authorizationRequest = new HttpRequestMessage(
+            HttpMethod.Post,
+            new Uri(_keyclokOptions.TokenUrl))
+        {
+            Content = authorizationRequestContent
+        };
+
+        var authorizationResponse = await base.SendAsync(authorizationRequest, cancellationToken);
+
+        authorizationResponse.EnsureSuccessStatusCode();
+
+        return await authorizationResponse.Content.ReadFromJsonAsync<AuthorizationToken>() ?? 
+            throw new ApplicationException();
+    }
+}
---
+++ src/Bookify.Infrastructure/Authentication/Models/KeycloakOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookify.Infrastructure.Authentication.Models;
+
+public sealed class KeycloakOptions
+{
+    public string AdminUrl { get; set; } = string.Empty;
+
+    public string TokenUrl { get; set; } = string.Empty;
+
+    public string AdminClientId { get; set; } = string.Empty;
+
+    public string AdminClientSecret { get; set; } = string.Empty;
+
+    public string AuthClientId { get; set; } = string.Empty;
+
+    public string AuthClientSecret { get; set; } = string.Empty;
+
+}
